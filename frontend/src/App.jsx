3d043f95aa9a21b30a95003c2ba366375e3da6cb
Антон Андрющенко <lazarenkoanton527@gmail.com>
2023-08-11 19:00:33 +0300

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
-import React, {useState} from 'react';
-import {Provider} from 'react-redux';
-import {Provider as RollbarProvider, ErrorBoundary} from '@rollbar/react';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import LoginForm from './routes/login';
 import ErrorPage from './errors/wrongRoute.js';
 import AuthContext from './contexts/index.js';
@@ -10,70 +10,70 @@ import useAuth from './Hooks/index.js';
 import store from './slices/index.js';
 import SignUp from './routes/SignUp';
 
-const AuthProvider = ({children}) => {
- const [logStatus, setStatus] = useState(!!localStorage.getItem('Token'));
+const AuthProvider = ({ children }) => {
+  const [logStatus, setStatus] = useState(!!localStorage.getItem('Token'));
 
- const logIn = () => {
-  setStatus(true);
- };
+  const logIn = () => {
+    setStatus(true);
+  };
 
- const logOut = () => {
-  localStorage.removeItem('Token');
-  setStatus(false);
- };
+  const logOut = () => {
+    localStorage.removeItem('Token');
+    setStatus(false);
+  };
 
- return (
-   <AuthContext.Provider value={{logStatus, logIn, logOut}}>
-    {children}
-   </AuthContext.Provider>
- );
+  return (
+    <AuthContext.Provider value={{ logStatus, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-const ChatRoute = ({children}) => {
- const {logStatus} = useAuth();
+const ChatRoute = ({ children }) => {
+  const { logStatus } = useAuth();
 
- return (
-   logStatus ? children : <Navigate to="/login"/>
- );
+  return (
+    logStatus ? children : <Navigate to="/login" />
+  );
 };
 
 const rollbarConfig = {
- accessToken: '8266f58662e741418e2bd7bea392e987',
- environment: 'testenv',
+  accessToken: '8266f58662e741418e2bd7bea392e987',
+  environment: 'testenv',
 };
 
 const App = () => {
- const route = createBrowserRouter([
-  {
-   path: '/',
-   element: (
-     <ChatRoute>
-      <Provider store={store}>
-       <Chat/>
-      </Provider>
-     </ChatRoute>
-   ),
-   errorElement: <ErrorPage/>,
-  },
-  {
-   path: '/login',
-   element: <LoginForm/>,
-  },
-  {
-   path: '/signup',
-   element: <SignUp/>,
-  },
- ]);
+  const route = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <ChatRoute>
+                <Provider store={store}>
+                    <Chat />
+                </Provider>
+              </ChatRoute>
+      ),
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/login',
+      element: <LoginForm />,
+    },
+    {
+      path: '/signup',
+      element: <SignUp />,
+    },
+  ]);
 
- return (
-   <AuthProvider>
-    <RollbarProvider config={rollbarConfig}>
-     <ErrorBoundary>
-      <RouterProvider router={route}/>
-     </ErrorBoundary>
-    </RollbarProvider>
-   </AuthProvider>
- );
+  return (
+    <AuthProvider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <RouterProvider router={route} />
+        </ErrorBoundary>
+      </RollbarProvider>
+    </AuthProvider>
+  );
 };
 
 export default App;

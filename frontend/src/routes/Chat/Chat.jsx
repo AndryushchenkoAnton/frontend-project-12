4c5a7b65b819fc3c2905ff25f37cf6ab320145d7
Антон Андрющенко <lazarenkoanton527@gmail.com>
2023-08-11 23:37:51 +0300

    fixes

--- frontend/src/routes/Chat/Chat.jsx
+++ frontend/src/routes/Chat/Chat.jsx
@@ -236,8 +236,8 @@ const Chat = () => {
         </div>
       </div>
       {modalDelete
-        ? (
-           <ModalDelete
+        ? (<
+            ModalDelete
             show={modalDelete}
             closeHandler={closeDelete}
             idToDelete={modalChId}

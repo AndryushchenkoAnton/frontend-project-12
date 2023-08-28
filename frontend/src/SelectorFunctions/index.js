305421d45794aa566b929a7b46926ed5eb6c8df2
Антон Андрющенко <lazarenkoanton527@gmail.com>
2023-08-29 01:24:05 +0300

    7 - 10 fixes

--- frontend/src/SelectorFunctions/index.js
+++ frontend/src/SelectorFunctions/index.js
@@ -2,10 +2,11 @@ import { useSelector } from 'react-redux';
 import { selectors as messagesSelectors } from '../Slices/messagesSlice.js';
 import { selectors as channelsSelectors } from '../Slices/channelsSlice.js';
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const getChannelById = (id) => useSelector((state) => channelsSelectors.selectById(state, id));
-
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const getMessages = () => Object.values(useSelector(messagesSelectors.selectEntities));
-
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const getChannels = () => Object.values(useSelector(channelsSelectors.selectEntities));
 
 export { getChannels, getChannelById, getMessages };

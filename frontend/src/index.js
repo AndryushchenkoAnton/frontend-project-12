5ca0b8b39098c5529d2539c943d15604dcf22d07
Anton Andriushchenko <lazarenkoanton527@gmail.com>
2024-03-14 19:33:23 +0100

    fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import localeInit from './init.jsx';
+import localeInit from './init.js';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
